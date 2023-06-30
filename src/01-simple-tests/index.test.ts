16c93c0f3e4b6909307842e85d0c0fe00b8817de
Anton Pomogaev <antonpomogaev@gmail.com>
2023-06-30 22:11:30 +0300

    feat: add task 1 solution

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,74 @@
 // Uncomment the code below and write your tests
 // import { simpleCalculator, Action } from './index';
 
+import { Action, simpleCalculator } from '01-simple-tests';
+
 describe('simpleCalculator tests', () => {
+  const [a, b] = [5, 5];
+
   test('should add two numbers', () => {
-    // Write your test here
+    const addition = simpleCalculator({ a, b, action: Action.Add });
+
+    expect(addition).toEqual(10);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const subtraction = simpleCalculator({
+      a,
+      b,
+      action: Action.Subtract,
+    });
+
+    expect(subtraction).toEqual(0);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const multiplication = simpleCalculator({
+      a,
+      b,
+      action: Action.Multiply,
+    });
+
+    expect(multiplication).toEqual(25);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const division = simpleCalculator({
+      a,
+      b,
+      action: Action.Divide,
+    });
+
+    expect(division).toEqual(1);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const exponention = simpleCalculator({
+      a,
+      b,
+      action: Action.Exponentiate,
+    });
+
+    expect(exponention).toEqual(3125);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const invalidAction = simpleCalculator({
+      a,
+      b,
+      action: 'InvalidAction',
+    });
+
+    expect(invalidAction).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const invalidArgsMultiplication = simpleCalculator({
+      a: 'invalidA',
+      b: 'invalidB',
+      action: Action.Multiply,
+    });
+
+    expect(invalidArgsMultiplication).toBeNull();
   });
 });
