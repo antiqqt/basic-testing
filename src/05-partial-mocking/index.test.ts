2683c4d47d401b72702a98b6d7ba0f004dc09cc5
Anton Pomogaev <antonpomogaev@gmail.com>
2023-07-01 11:00:51 +0300

    feat: add solution to task 5

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,8 +1,20 @@
-// Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+const [mockOneText, mockTwoText, mockThreeText] = [
+  'mockOneText',
+  'mockTwoText',
+  'mockThreeText',
+];
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const originalModule =
+    jest.requireActual<typeof import('./index')>('./index');
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    mockOne: jest.fn(() => mockOneText),
+    mockTwo: jest.fn(() => mockTwoText),
+    mockThree: jest.fn(() => mockThreeText),
+  };
 });
 
 describe('partial mocking', () => {
@@ -10,11 +22,26 @@ describe('partial mocking', () => {
     jest.unmock('./index');
   });
 
-  test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
+  test('mockOne, mockTwo, mockThree should not log into console', async () => {
+    const { mockOne, mockThree, mockTwo } = await import('./index');
+    console.log = jest.fn();
+
+    expect(mockOne()).toBe(mockOneText);
+    expect(mockTwo()).toBe(mockTwoText);
+    expect(mockThree()).toBe(mockThreeText);
+
+    expect(mockOne).toBeCalled();
+    expect(mockTwo).toBeCalled();
+    expect(mockThree).toBeCalled();
+
+    expect(console.log).not.toBeCalled();
   });
 
-  test('unmockedFunction should log into console', () => {
-    // Write your test here
+  test('unmockedFunction should log into console', async () => {
+    const { unmockedFunction } = await import('./index');
+    console.log = jest.fn();
+
+    expect(unmockedFunction()).toBeUndefined();
+    expect(console.log).toBeCalled();
   });
 });
